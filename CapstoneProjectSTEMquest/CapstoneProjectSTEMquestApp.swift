d5495ae910ed065a0bc83ed0ef0092cc810b41ec
Khushi Jain <khushijain14908@gmail.com>
2025-07-29 11:38:34 -0700

    Initial Commit

---
+++ CapstoneProjectSTEMquest/CapstoneProjectSTEMquestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectSTEMquestApp.swift
+//  CapstoneProjectSTEMquest
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectSTEMquestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
