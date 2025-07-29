d8c58ddc01d1151a173ffaf4711472868c78a775
Montserrat <jaureguimontse16@gmail.com>
2025-07-29 13:26:12 -0700

    i added a title and some placeholder text for the challenges

---
+++ CapstoneProjectSTEMquest/ChallengesBasicView.swift
@@ -0,0 +1,50 @@
+//
+//  ChallengesBasicView.swift
+//  CapstoneProjectSTEMquest
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct ChallengesBasicView: View {
+    var body: some View {
+        Text("Challenges for Today")
+            .font(.system(size: 30))
+            .fontWeight(.black)
+        Spacer()
+        
+        
+        
+        
+        Text("Challenge 1")
+        Text("Challenge 1 description")
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        
+        Text("Challenge 2")
+        Text("Challenge 2 description + image")
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        Spacer()
+        
+        Text("Challenge 3")
+        Text("Challenge 3 description + image")
+        Spacer()
+    }
+}
+
+#Preview {
+    ChallengesBasicView()
+}
