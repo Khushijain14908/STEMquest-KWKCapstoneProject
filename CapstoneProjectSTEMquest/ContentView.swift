1a89994e886b99d08a2acaaf8000503f207f9a45
Jenny <jennyluv1209@gmail.com>
2025-07-31 11:27:39 -0700

    added navigation stack and new page (SignUpView)

--- CapstoneProjectSTEMquest/ContentView.swift
+++ CapstoneProjectSTEMquest/ContentView.swift
@@ -15,59 +15,61 @@ struct ContentView: View {
     @State private var showSignUp = false
     
     var body: some View {
-        ScrollView{
-            VStack(spacing: 20) {
-                Text("STEMquest")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.top, 40)
-                
-                Image(systemName: "person.crop.circle.fill")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.gray)
+        NavigationStack {
+            ScrollView{
+                VStack(spacing: 20) {
+                    Text("STEMquest")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.top, 40)
                     
-                TextField("Username", text: $username)
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .autocapitalization(.none)
-                
-                SecureField("Password", text: $password)
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                
-                Button("Login") {
-                    isLoggedIn = true
-                }
-                
-                Button(action: {
-                    showSignUp = true
-                }) {
-                Text("Don't have an account?\nCreate an account")
-                        .font(.footnote)
-                        .foregroundColor(.blue)
-                        .multilineTextAlignment(.center)
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.gray)
+                    
+                    TextField("Username", text: $username)
                         .padding()
-    
-                }
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .autocapitalization(.none)
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                    
+                    Button("Login") {
+                        isLoggedIn = true
+                    }
+                    
+                    Button(action: {
+                        showSignUp = true
+                    }) {
+                        Text("Don't have an account?\nCreate an account")
+                            .font(.footnote)
+                            .foregroundColor(.blue)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        
+                    }
                     Spacer()
-            
+                    
                     
                 }
+                .padding()
+                .fullScreenCover(isPresented: $showSignUp) {
+                    SignUpView()
+                    
+                }
+                .fullScreenCover(isPresented: $isLoggedIn, content: {
+                })
+            }
             .padding()
-            .fullScreenCover(isPresented: $showSignUp) {
-                
-            }
-            .fullScreenCover(isPresented: $isLoggedIn, content: {
-            })
-            }
-        .padding()
-        Spacer()
+            Spacer()
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
---
+++ SignUpView.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpView.swift
+//  CapstoneProjectSTEMquest
+//
+//  Created by Jenny Paek on 7/31/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        
+
+    }
+}
+
+#Preview {
+    SignUpView()
+}
