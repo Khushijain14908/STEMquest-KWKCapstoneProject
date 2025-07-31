980697b30e30ee29017eead6beea19f9d95f9152
Montserrat <jaureguimontse16@gmail.com>
2025-07-31 10:50:09 -0700

    i added the description to the challeneges

--- CapstoneProjectSTEMquest/ChallengesBasicView.swift
+++ CapstoneProjectSTEMquest/ChallengesBasicView.swift
@@ -24,13 +24,11 @@ struct ChallengesBasicView: View {
                         .padding(.horizontal, 10)
                 }
                 
-                Text("Make a Balloon Car! - Build a balloon-powered car from recycled materials in this fun activity!")
+                Text("Make a Balloon Car! - Build a balloon-powered car from recycled materials in this fun activity! Make two to race them!")
                     .font(.system(size: 17))
                     .fontWeight(.black)
                     .multilineTextAlignment(.leading)
                     .padding(.horizontal, 10)
-                
-                
                 
                 Image("balloon car")
                     .resizable()
@@ -45,6 +43,12 @@ struct ChallengesBasicView: View {
                         .padding(.horizontal, 10)
                 }
                 
+                Text("Have you ever wanted to send your friend a secret message that no-one else can read? Then you might know of invisable ink a type of ink that doesn't show on paper until you put a special treatment on it.")
+                    .font(.system(size: 17))
+                    .fontWeight(.black)
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal, 10)
+                
                 Image("invisable ink")
                     .resizable()
                     .scaledToFit()
@@ -52,6 +56,32 @@ struct ChallengesBasicView: View {
                 
                 Text("DIY Robot Hands")
                     .padding(.horizontal, 10)
+                    .font(.system(size: 20))
+                    .fontWeight(.black)
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal, 10)
+                
+                
+                
+                
+                
+                
+                
+                
+                Text("Build these simple diy robot hands and try to write or pick things up ")
+                    .padding(.horizontal, 10)
+                    .font(.system(size: 20))
+                    .fontWeight(.black)
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal, 10)
+                
+                
+                
+                
+                
+                
+                
+                
                 
                 Image("diy robot hands")
                     .resizable()
