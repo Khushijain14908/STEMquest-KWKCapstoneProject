ca30136253e86a84dbbb4d9d5903328a4171864a
Jenny <jennyluv1209@gmail.com>
2025-07-31 11:54:43 -0700

    added SignUpView information (username, password, email, etc.)

--- SignUpView.swift
+++ SignUpView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var username = ""
+    @State private var password = ""
+    @State private var email = ""
+    @State private var firstName = ""
+    @State private var LastName = ""
+    @State private var phoneNumber = ""
+    
     var body: some View {
-        
-                }
+        VStack(spacing: 15) {
+            Text("Sign Up!")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Group {
+                TextField("Username", text: $username)
+                SecureField("Password", text: $password)
+                TextField("Email", text: $email)
+                TextField("First Name", text: $firstName)
+                TextField("Last Name", text: $LastName)
+                TextField("Phone Number", text: $phoneNumber)
             }
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            
+            Button("Create Account") {
+                dismiss()
+            }
+        }
+    }
+}
         
 
 #Preview {
